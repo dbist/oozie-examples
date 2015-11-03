f729f8c66ce1551c74b7325c8e27ca06ab353eca
Artem Ervits <generic13@gmail.com>
2015-11-03 16:44:35 -0500

    added sample shell workflow example

---
+++ apps/shell/scripts/script.sh
@@ -0,0 +1,5 @@
+echo "I AM: `whoami`"
+echo "Running On: `hostname`"
+echo "CWD: `pwd`"
+echo "Can I see these clients? Hive = `which hive`, Sqoop = `which sqoop`"
+echo "`ls -l /usr/hdp/current/sqoop-client/ | head -n 5`"
